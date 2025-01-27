7070c7b27c06f36e819017fa144970135b5c0f5f
nedisss <wasnaver07@gmail.com>
2025-01-27 18:00:56 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-const canvas = document.getElementById("gameCanvas");
-const ctx = canvas.getContext("2d");
-
-canvas.width = 400;
-canvas.height = 400;
-
-ctx.fillStyle = "lime";
-ctx.fillRect(50, 50, 20, 20); // Piešiame gyvatės segmentą
+document.getElementById('start-button').addEventListener('click', function() {
+  document.getElementById('game-area').innerHTML = '<h2>Žaidimas prasideda!</h2>';
+});
